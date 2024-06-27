f5521f6b61acbda7d4e081810d43f75432304cc1
Evgeny1r <92919954+Evgeny1r@users.noreply.github.com>
2024-06-27 03:11:26 +0300

    Update Main.java

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -2,7 +2,16 @@
 import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
+    public static String calc(){
+        System.out.printf("Hello and welcome!");
+        System.out.printf("\n");
+        System.out.printf("Input the equation");
+        System.out.printf("\n");
+        Scanner in1 = new Scanner(System.in);
+        String e2 = in1.nextLine();
+        return e2;
+    }
+    public static void main(String[] args) throws Exception {
         int a=0; //первое число
         int b=0;//2е число
         int p;//+
@@ -17,13 +26,7 @@ public class Main {
         String[] roman1 = {"8", "3", "7", "4", "6", "9", "10", "2", "1", "5"};
         String[] roman3 = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
         };
-        Scanner in = new Scanner(System.in);
-        System.out.printf("Hello and welcome!");
-        System.out.printf("\n");
-        System.out.printf("Input the equation");
-        System.out.printf("\n");
-        String e1 = in.nextLine();
-        String e = e1.replaceAll(" ", "");
+        String e = calc().replaceAll(" ", "");
         p = e.indexOf("+");
         m = e.indexOf("-");
         u = e.indexOf("*");
@@ -34,102 +37,101 @@ public class Main {
         int countr = e.length() - e.replace(String.valueOf("/"), "").length();
         int count=countp+countm+countu+countr;
 
-            String[] arr = e.split("[+\\-*/]");
+        String[] arr = e.split("[+\\-*/]");
+        if(count!=1){
+            throw new Exception("fatal error");
+        }
 
+        for (int i=0; i<roman.length; i++){
 
-            for (int i=0; i<roman.length; i++){
-
-                if(roman[i].equals(arr[0])){
-                    a=Integer.parseInt(roman1[i]);
-                    r1=1;
-                    break;
-                }
-            }
-            if (a==0){
-                try{
-                    a=Integer.parseInt(arr[0]);
-                }
-                catch (NumberFormatException ex){
-                    ex.printStackTrace();
-                }
-            }
-            for (int i=0; i<roman.length; i++){
-
-                if(roman[i].equals(arr[1])){
-                    b=Integer.parseInt(roman1[i]);
-                    r2=1;
-                    break;
-                }
-            }
-            if (b==0){
-                try{
-                    b=Integer.parseInt(arr[1]);
-                }
-                catch (NumberFormatException ex){
-                    ex.printStackTrace();
-                }
-                }
-
-
-            if((a>0)&&(b>0)&&(r1==r2)&&(a<11)&&(b<11)&&(count==1)){
-                if(r1==0){
-                    if(p>0){
-                        System.out.println(a+b);
-                    }
-                    else if (m>0) {
-                        System.out.println(a-b);
-                    }
-                    else if (u>0){
-                        System.out.println(a*b);
-                    }
-                    else if (r>0){
-                        if(((float)a/(float)b) % 1 == 0){
-                            System.out.println(a/b);
-                        }
-                        else {
-                            Ex+=1;
-                        }
-
-                    }
-                    else {
-                        Ex+=1;
-                    }
-                }
-                if(r1==1){
-                    if(p>0){
-                        int x = a+b;
-                        System.out.println(roman3[x]);
-                    }
-                    else if ((m>0)&&(a-b>0)) {
-                        int x = a-b;
-                        System.out.println(roman3[x]);
-                    }
-                    else if (u>0){
-                        int x = a*b;
-                        System.out.println(roman3[x]);
-                    }
-                    else if (r>0){
-                        if(((float)a/(float)b) % 1 == 0){
-                            int x = a/b;
-                            System.out.println(roman3[x]);
-                        }
-                        else {
-                            Ex+=1;
-                        }
-
-                    }
-                    else {
-                        Ex+=1;
-                    }
-                }
-
-            }
-            else {
-                Ex+=1;
-            }
-            if(Ex>0){
-                String test = null;
-                test.toString();
+            if(roman[i].equals(arr[0])){
+                a=Integer.parseInt(roman1[i]);
+                r1=1;
+                break;
             }
         }
+        if (a==0){
+            try{
+                a=Integer.parseInt(arr[0]);
+            }
+            catch (NumberFormatException ex){
+                throw new Exception("fatal error");
+            }
+        }
+        for (int i=0; i<roman.length; i++){
+
+            if(roman[i].equals(arr[1])){
+                b=Integer.parseInt(roman1[i]);
+                r2=1;
+                break;
+            }
+        }
+        if (b==0){
+            try{
+                b=Integer.parseInt(arr[1]);
+            }
+            catch (NumberFormatException ex){
+                throw new Exception("fatal error");
+            }
+        }
+
+
+        if((a>0)&&(b>0)&&(r1==r2)&&(a<11)&&(b<11)&&(count==1)){
+            if(r1==0){
+                if(p>0){
+                    System.out.println(a+b);
+                }
+                else if (m>0) {
+                    System.out.println(a-b);
+                }
+                else if (u>0){
+                    System.out.println(a*b);
+                }
+                else if (r>0){
+                    if(((float)a/(float)b) % 1 == 0){
+                        System.out.println(a/b);
+                    }
+                    else {
+                        throw new Exception("fatal error");
+                    }
+
+                }
+                else {
+                    throw new Exception("fatal error");
+                }
+            }
+            if(r1==1){
+                if(p>0){
+                    int x = a+b;
+                    System.out.println(roman3[x]);
+                }
+                else if ((m>0)&&(a-b>0)) {
+                    int x = a-b;
+                    System.out.println(roman3[x]);
+                }
+                else if (u>0){
+                    int x = a*b;
+                    System.out.println(roman3[x]);
+                }
+                else if (r>0){
+                    if(((float)a/(float)b) % 1 == 0){
+                        int x = a/b;
+                        System.out.println(roman3[x]);
+                    }
+                    else {
+                        throw new Exception("fatal error");
+                    }
+
+                }
+                else {
+                    throw new Exception("fatal error");
+                }
+            }
+
+        }
+        else {
+            throw new Exception("fatal error");
+        }
+
     }
+}
